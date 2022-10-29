dd45b9d7a98472e3a4dba43fb3aec7f62f8f7c6a
ashley <o60431y@gmail.com>
2022-10-30 04:35:31 +0800

    feat(main): add list tasks

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_pydantic import validate
+from schema.response import MultiResponse
+from schema.response.task import Task
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# init task data
+tasks = [{"id": 1, "name": "name", "status": 0}]
+
+@app.route("/tasks")
+@validate()
+def list_tasks():
+    """
+    取得任務列表
+    """
+    return MultiResponse[Task](result=tasks)
