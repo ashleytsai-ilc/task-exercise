3efd169cd824714f072d82550a685bbcdd54412a
ashley <o60431y@gmail.com>
2022-10-30 05:06:34 +0800

    feat(main): add create task

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from http import HTTPStatus
 from flask import Flask
 from flask_pydantic import validate
-from schema.response import MultiResponse
+
+from schema import TaskStatus
+from schema.response import MultiResponse, Response
 from schema.response.task import Task
+from schema.request.task import CreateTask
 
 app = Flask(__name__)
 
@@ -16,3 +20,14 @@ def list_tasks():
     取得任務列表
     """
     return MultiResponse[Task](result=tasks)
+
+@app.route("/task", methods=["POST"])
+@validate(on_success_status=HTTPStatus.CREATED)
+def create_task(body: CreateTask):
+    """
+    建立任務
+    """
+    task = {"id": len(tasks) + 1, "name": body.name, "status": TaskStatus.INCOMPLETE}
+    tasks.append(task)
+
+    return Response[Task](result=task)
